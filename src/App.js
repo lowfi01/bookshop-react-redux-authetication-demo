64803bd23ba3a65f2e716b807a4c9482b7172970
DESKTOP-85TAR51\james <jamesherrero@gmail.com>
2018-02-25 18:18:59 +1100

    React router setup with basic pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route } from "react-router-dom";
+import Homepage from "./components/HomePage";
+import LoginPage from "./components/LoginPage";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = props => (
+  <div>
+    <Route exact path="/" component={Homepage} />
+    <Route exact path="/login" component={LoginPage} />
+  </div>
+);
 
 export default App;
---
+++ src/components/LoginPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const LoginPage = props => (
+  <div>
+    <h1>LoginPage</h1>
+  </div>
+);
+
+export default LoginPage;
