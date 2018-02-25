cf1029182c42a8b6e689c7bab6346f215a8a486c
DESKTOP-85TAR51\james <jamesherrero@gmail.com>
2018-02-25 20:24:27 +1100

    create post request for login - confirm with network + payload

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,20 +1,29 @@
 import React from "react";
 import LoginForm from "../forms/LoginForm";
+import { PropTypes } from "prop-types";
+import { connect } from "react-redux";
+import { login } from "../../actions/auth";
 
 class LoginPage extends React.Component {
-  submit = data => {
-    console.log(data);
-  };
+  submit = data =>
+    this.props.login(data).then(e => this.props.history.push("/"));
 
   render() {
     return (
       <div>
         <h1>LoginPage</h1>
-
         <LoginForm submit={this.submit} />
       </div>
     );
   }
 }
 
-export default LoginPage;
+LoginPage.proptypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired
+  }).isRequired
+};
+
+export default connect(null, {
+  login
+})(LoginPage);
